ef13bfeb0574c044e01f925dba905e60c968196f
dayeonnam <nam44@purdue.edu>
2022-02-08 11:44:12 +0900

    app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { NavContainer, NavElement } from './style/navbar';
+import {MainContainer, ExperienceContainer, ProjectContainer, HomeContainer, SkillContainer} from './style/main_block';
 
 function App() {
+  const menuItems = ["Home", "Project", "Skill", "Experience"];
+  const [clicked, setClicked] = useState("");
+
+  const handleClick = (item) => {
+    setClicked(item);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <MainContainer>
+    <NavContainer>
+      {menuItems.map((elem, i) => (
+          <NavElement
+            key={i}
+            focused={clicked === elem}
+            onClick={() => handleClick(elem)}
+          >
+            {elem}
+          </NavElement>
+      ))}
+    </NavContainer>
+    <HomeContainer>
+
+    </HomeContainer>
+
+    <ExperienceContainer>
+
+    </ExperienceContainer>
+
+    <ProjectContainer>
+
+    </ProjectContainer>
+
+    <SkillContainer>
+
+    </SkillContainer>
+
+  </MainContainer>
   );
 }
 
